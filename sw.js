84aad882a5c5c7586777e9d57d07033455f70ef7
RC Neruh Ignacio <fckneruzxc@gmail.com>
2026-04-24 06:54:07 +0800

    Activity 15: PWA conversion

---
+++ sw.js
@@ -0,0 +1,25 @@
+const CACHE_NAME = 'addu-alumni-v1';
+const assetsToCache = [
+  './',
+  './index.html',
+  './main.js',
+  './aknight-01.png'
+];
+
+// Install Service Worker
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(assetsToCache);
+    })
+  );
+});
+
+// Fetch Strategy: Cache First, then Network
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
